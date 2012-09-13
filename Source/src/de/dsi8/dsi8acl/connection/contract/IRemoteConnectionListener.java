43652b6ea258c6124213ff0dc35a6901e2ce9b44
svento <mail@sven.to>
2012-09-14 01:05:19 +0200

    connection und communication initial import

---
+++ Source/src/de/dsi8/dsi8acl/connection/contract/IRemoteConnectionListener.java
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ * Copyright (C) 2012 Sven Nobis
+ * 
+ * This file is part of AndroidRCCar (http://androidrccar.sven.to)
+ * 
+ * AndroidRCCar is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This source code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *  
+ * You should have received a copy of the GNU General Public License
+ * along with this source code; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ ******************************************************************************/
+package de.dsi8.dsi8acl.connection.contract;
+
+import de.dsi8.dsi8acl.connection.impl.TCPConnection;
+import de.dsi8.dsi8acl.connection.model.Message;
+
+/**
+ * The {@link TCPConnection} uses this listener to inform the implementor of this interface 
+ * that something happens. (Usually {@link AbstractLogic})
+ * @author sven
+ *
+ */
+public interface IRemoteConnectionListener {
+	
+	/**
+	 * Called from the {@link TCPConnection} when a message is received.
+	 * @param message The message.
+	 */
+	void messageReceived(Message message);
+	
+	/**
+	 * Called from the {@link TCPConnection} when a problem with the exception occurs.
+	 * @param ex The Problem
+	 */
+	void connectionProblem(Exception ex);
+}
---
+++ Source/src/de/dsi8/dsi8acl/connection/impl/TCPConnection.java
@@ -0,0 +1,134 @@
+/*******************************************************************************
+ * Copyright (C) 2012 Sven Nobis
+ * 
+ * This file is part of AndroidRCCar (http://androidrccar.sven.to)
+ * 
+ * AndroidRCCar is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This source code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *  
+ * You should have received a copy of the GNU General Public License
+ * along with this source code; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ ******************************************************************************/
+package de.dsi8.dsi8acl.connection.impl;
+
+import java.io.IOException;
+import java.net.Socket;
+
+import android.os.Handler;
+import android.os.Looper;
+
+import com.fasterxml.jackson.core.JsonGenerator.Feature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import de.dsi8.dsi8acl.connection.contract.IRemoteConnection;
+import de.dsi8.dsi8acl.connection.contract.IRemoteConnectionListener;
+import de.dsi8.dsi8acl.connection.model.Message;
+
+/**
+ * This class handles the communication with the other Android device (Host or Client).
+ * It serialize the message to send into JSON
+ * and deserialize the received messages.
+ * @author sven
+ */
+public class TCPConnection implements IRemoteConnection {
+	/**
+	 *  A TCP socket to the other Android device (Host/Client).
+	 */
+	private final Socket socket;
+	/**
+	 * Will be informed, when something happen here.
+	 * @see IRemoteConnectionListener
+	 */
+	private final IRemoteConnectionListener listener;
+	/**
+	 * @see MessageListenerThread
+	 */
+	private final MessageListenerThread messageListenerThread;
+	/**
+	 * For (De-)Serialization of {@link Message}s into JSON.
+	 */
+	private final ObjectMapper jsonMapper = new ObjectMapper();
+	
+	/**
+	 * Default Constructor.
+	 * @param dependencyContainer The {@link Socket}
+	 * @param listener Will be informed, when something happen here.
+	 */
+	public TCPConnection(Socket socket, IRemoteConnectionListener listener) { 
+		this.socket = socket;
+		this.listener = listener;
+		jsonMapper.configure(Feature.AUTO_CLOSE_TARGET, false);
+		jsonMapper.configure(SerializationFeature.CLOSE_CLOSEABLE, false);
+		jsonMapper.configure(SerializationFeature.FLUSH_AFTER_WRITE_VALUE, true);
+		messageListenerThread = new MessageListenerThread(this, new Handler(Looper.getMainLooper()));
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void startMessageListener() {
+		messageListenerThread.start();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void close() throws IOException {
+		messageListenerThread.interrupt();
+		if(!socket.isClosed())
+		{
+			socket.close();
+		}
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void sendMessage(Message message) throws IOException {
+		jsonMapper.writeValue(socket.getOutputStream(), message);
+	}
+	
+	/**
+	 * Inform the {@link IRemoteConnectionListener} about a new message.
+	 * @param message a new message.
+	 */
+	void messageRecived(Message message) {
+		listener.messageReceived(message);
+	}
+	
+	/**
+	 * Inform the {@link IRemoteConnectionListener} that something went wrong.
+	 * @param e What went wrong.
+	 */
+	void connectionProblem(Exception e) {
+		listener.connectionProblem(e);
+	}
+	
+	/**
+	 * For (De-)Serialization of {@link Message}s into JSON.
+	 * @return A {@link ObjectMapper}
+	 */
+	ObjectMapper getJsonObjectMapper() {
+		return jsonMapper;
+	}
+	
+	/**
+	 * Connection to the partner.
+	 * @return A {@link Socket}
+	 */
+	Socket getSocket() {
+		return socket;
+	}
+}
